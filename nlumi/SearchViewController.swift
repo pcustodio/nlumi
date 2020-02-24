316b951e5783596ef86741f89983ba9a4a50abb6
Paulo Custódio <paulo.custodio@gmail.com>
2020-02-24 18:45:53 +0000

    Basic Json + DetailViewController + launchscreen

--- nlumi/SearchViewController.swift
+++ nlumi/SearchViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
