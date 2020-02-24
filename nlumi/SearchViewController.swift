75d136ff65f8024695ff9e2d45a42f44d3913e40
Paulo Custódio <paulo.custodio@gmail.com>
2020-02-24 18:12:29 +0000

    Initial Commit

---
+++ nlumi/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  nlumi
+//
+//  Created by Paulo Custódio on 24/02/2020.
+//  Copyright © 2020 Paulo Custódio. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
