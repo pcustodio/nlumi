c6071dfdb819211a213cc515a95e9d910b40944f
Paulo Custódio <paulo.custodio@gmail.com>
2020-02-25 18:07:51 +0000

    Customise cells

--- nlumi/Controllers/SearchViewController.swift
+++ nlumi/Controllers/SearchViewController.swift
@@ -13,11 +13,13 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     let data = DictionaryLoader().dictionary
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //print json
-        print(data)
+        //print(data)
         
         //trigger UITableViewDataSource
         tableView.dataSource = self
@@ -36,9 +38,17 @@ extension SearchViewController: UITableViewDataSource {
     //this is where we are creating our cell
     //indexpath is the position: which cell it should display on each row of our table view
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = data[indexPath.row].pt
-        print(cell)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+        
+        let pt = data[indexPath.row].pt
+        let gramatica = data[indexPath.row].gramatica
+        let changana = data[indexPath.row].changana
+        let macua = data[indexPath.row].macua
+        let xironga = data[indexPath.row].xironga
+        
+        cell.textLabel?.text = pt
+        cell.detailTextLabel?.text = "(\(gramatica)) \(changana)"
         return cell
     }
 }
+
