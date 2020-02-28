33e7c92bdca386d8e8b5dee7988e2e5a1a96d248
Paulo Custódio <paulo.custodio@gmail.com>
2020-02-28 11:55:47 +0000

    Fine-tuning Favorites

--- nlumi/Controllers/DetailViewController.swift
+++ nlumi/Controllers/DetailViewController.swift
@@ -16,14 +16,13 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var translationLabel: UILabel!
     @IBOutlet weak var languageLabel: UILabel!
     @IBOutlet weak var annotationMarker: UILabel!
+    @IBOutlet weak var bookmarkLabel: UIBarButtonItem!
     
     var ptWord = ""
     var trWord = ""
     var laWord = ""
     var grWord = ""
-    
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,16 +30,32 @@ class DetailViewController: UIViewController {
         grammarLabel.text = grWord
         translationLabel.text = trWord
         languageLabel.text = laWord
+        annotationMarker.text = ""
     
         retrieveData()
     }
 
-    
     @IBAction func addBookmark(_ sender: UIBarButtonItem) {
-        createData()
-        retrieveData()
+        if self.bookmarkLabel.image == UIImage(systemName: "bookmark.fill") {
+            print("yo we are a not favorite atm")
+            deleteData()
+            retrieveData()
+        } else {
+            print("yo we are a favorite atm")
+            createData()
+            retrieveData()
+            successAnimated()
+        }
     }
     
+    func successAnimated() {
+        //success message
+        annotationMarker.text = "Anotado"
+        annotationMarker.isHidden = false
+        UIView.animate(withDuration: 0.5, delay: 2.0, animations: { () -> Void in
+            self.annotationMarker.alpha = 0
+        })
+    }
     
     //MARK: - Create Data
     func createData(){
@@ -72,42 +87,84 @@ class DetailViewController: UIViewController {
     
     func retrieveData() {
             
-            //As we know that container is set up in the AppDelegates so we need to refer that container.
-            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-            
-            //We need to create a context from this container
-            let managedContext = appDelegate.persistentContainer.viewContext
-            
-            //Prepare the request of type NSFetchRequest  for the entity
-            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Annotations")
+        //As we know that container is set up in the AppDelegates so we need to refer that container.
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        //We need to create a context from this container
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        //Prepare the request of type NSFetchRequest  for the entity
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Annotations")
             
     //        fetchRequest.fetchLimit = 1
     //        fetchRequest.predicate = NSPredicate(format: "username = %@", "Ankur")
     //        fetchRequest.sortDescriptors = [NSSortDescriptor.init(key: "email", ascending: false)]
 
-            do {
-                let result = try managedContext.fetch(fetchRequest)
-                for data in result as! [NSManagedObject] {
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            for data in result as! [NSManagedObject] {
+                
+                print(data.value(forKey: "ptNoted") as! String)
+                print(data.value(forKey: "trNoted") as! String)
+                
+                let retrievedData = data.value(forKey: "trNoted") as! String
+                
+                if retrievedData == "\(trWord)" {
+                    print("It is a Fav")
                     
-                    print(data.value(forKey: "ptNoted") as! String)
-                    print(data.value(forKey: "trNoted") as! String)
+
+
+                    //change bookmark icon
+                    self.bookmarkLabel.image = UIImage(systemName: "bookmark.fill")
                     
-                    let retrievedData = data.value(forKey: "ptNoted") as! String
-                    
-                    if retrievedData == "\(ptWord)" {
-                        print("It is a Fav")
-                        annotationMarker.text = "Anotado"
-                        
-                    } else {
-                        print("Not a Fav")
-                        annotationMarker.text = ""
-                    }
+                } else {
+                    print("Not a Fav")
+                    annotationMarker.text = ""
                 }
-                
-            } catch {
-                
-                print("Failed")
             }
+            
+        } catch {
+            
+            print("Failed")
         }
+    }
+    
+    //MARK: - Delete Data
+    
+    func deleteData(){
+        
+        //As we know that container is set up in the AppDelegates so we need to refer that container.
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        //We need to create a context from this container
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Annotations")
+        fetchRequest.predicate = NSPredicate(format: "trNoted = %@", trWord)
+       
+        do
+        {
+            let test = try managedContext.fetch(fetchRequest)
+            
+            let objectToDelete = test[0] as! NSManagedObject
+            managedContext.delete(objectToDelete)
+            
+            //change bookmark icon
+            self.bookmarkLabel.image = UIImage(systemName: "bookmark")
+            
+            do{
+                try managedContext.save()
+            }
+            catch
+            {
+                print(error)
+            }
+            
+        }
+        catch
+        {
+            print(error)
+        }
+    }
     
 }
